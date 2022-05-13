6bf0f1434e6ea1ecac42ffcfa7a5f757592eeaa8
Reece Orbuck <reeceorbuck@me.com>
2022-05-13 22:00:48 +0800

    include map

--- generate.ts
+++ generate.ts
@@ -1,3 +1,7 @@
 const response = await fetch('https://cdnjs.cloudflare.com/ajax/libs/ajv/8.10.0/ajv7.min.js');
 const responseText = await response.text();
 await Deno.writeTextFile("./generated/ajv7.min.js", responseText + ";\nexport default Ajv;");
+
+const map = await fetch('https://cdnjs.cloudflare.com/ajax/libs/ajv/8.10.0/ajv7.min.js.map');
+const mapText = await map.text();
+await Deno.writeTextFile("./generated/ajv7.min.js.map", mapText);
